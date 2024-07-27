efca5db99a1e5fbddeee262cbf6c27432e6f1ca2
shaun macdougall <me@shaunmacdougall.com>
2024-07-27 18:55:22 +1000

    responsive header top, branding, nav open & close

--- header.php
+++ header.php
@@ -22,31 +22,49 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<div class="top-header">
+
+	<div class="top-header__shipping">
+		<p class="top-header__p"><i class="fa-solid fa-truck-fast"></i> Free shipping on all orders over $100</p>
+	</div>
+
+	<div class="top-header__cart">
+		<p class="top-header__p"><i class="fa-solid fa-cart-shopping"></i> Your cart: 0 items - $0.00</p>
+	</div>
+
+</div>  <!-- // #top-header -->
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'usedartsounds' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
+
 			the_custom_logo();
+
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<span><?php bloginfo( 'name' ); ?></span>
+					</a>
+				</h1>
 				<?php
 			else :
-				?>
+			?>
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
+			<?php
 			endif;
-			$used_art_soundsdescription = get_bloginfo( 'description', 'display' );
-			if ( $used_art_soundsdescription || is_customize_preview() ) :
+
+			$used_art_sounds_description = get_bloginfo( 'description', 'display' );
+			if ( $used_art_sounds_description || is_customize_preview() ) :
 				?>
-				<p class="site-description"><?php echo $used_art_soundsdescription; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="site-description"><?php echo $used_art_sounds_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'usedartsounds' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa-solid fa-bars"></i><i class="fa-solid fa-x"></i><span><?php esc_html_e( 'Primary Menu', 'usedartsounds' ); ?></span></button>
 			<?php
 			wp_nav_menu(
 				array(
